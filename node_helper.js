3854145692a4022c6c1d32d9ccb26104db244e1e
Cato Antonsen <cato.antonsen@gmail.com>
2017-01-19 22:50:08 +0100

    Fixed bug that preventet image to show when autoHide was false.

--- node_helper.js
+++ node_helper.js
@@ -10,21 +10,23 @@ var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
 	start: function() {
-		
 		console.log("Starting module: " + this.name);
-
-		var self = this;
-		this.expressApp.get('/motioneye', function (req, res) {
-			console.log("Motion registered");
-			res.send('Motion registered');
-			self.sendSocketNotification("MotionEyeShow", null);
-		});
 	},
 
 	socketNotificationReceived: function(notification, config) {
 		var self = this;
 		if (notification === "CONFIG") {
 			self.config = config;
+			
+			if (config.autoHide) {
+				this.expressApp.get('/motioneye', function (req, res) {
+					console.log("Motion registered");
+					res.send('Motion registered');
+					self.sendSocketNotification("MotionEyeShow", null);
+				});
+			} else {
+				self.sendSocketNotification("MotionEyeShow", null);
+			}
 			return;
 		}
 	},
